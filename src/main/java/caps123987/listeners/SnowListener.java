b6a8a12d9c29f80b9e64ad439c6b81f7f65f5379
CAPS123987 <hnilickakuba@gmail.com>
2023-12-18 22:41:20 +0100

    update

--- src/main/java/caps123987/listeners/SnowListener.java
+++ src/main/java/caps123987/listeners/SnowListener.java
@@ -1,13 +1,92 @@
 package caps123987.listeners;
 
+import caps123987.snowballs.SnowBalls;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.PotionSplashEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class SnowListener implements Listener {
+    public List<Player> players = new ArrayList<Player>();
+
     @EventHandler
     public void onSnowballHit(EntityDamageByEntityEvent e) {
-        Bukkit.broadcastMessage("Snowball hit!");
+        if(!(e.getDamager() instanceof Snowball)){
+            return;
+        }
+
+        if(!(e.getEntity() instanceof Player plKill)){
+            return;
+        }
+
+        if(!e.getCause().equals(DamageCause.PROJECTILE)){
+            return;
+        }
+
+        Snowball snow = (Snowball) e.getDamager();
+
+        if(!(snow.getShooter() instanceof Player damager)){
+            plKill.damage(2);
+            plKill.setVelocity(snow.getVelocity().multiply(2));
+            return;
+        }
+
+        damager.sendMessage("You hit " + plKill.getName() + " with a snowball!");
+        plKill.sendMessage("You were hit by " + damager.getName() + " with a snowball!");
+        plKill.damage(2);
+        plKill.setVelocity(snow.getVelocity().multiply(2));
+
+    }
+
+    @EventHandler
+    public void freeze(PotionSplashEvent e){
+        List<Player> playersSplash = e.getAffectedEntities().stream().filter(entity -> entity instanceof Player).map(entity -> (Player) entity).toList();
+
+
+
+        playersSplash.forEach(player -> {
+
+            if(!players.contains(player)){
+                players.add(player);
+
+                player.sendMessage("You have been frozen!");
+
+                Bukkit.getScheduler().scheduleSyncDelayedTask(SnowBalls.getInstance(),()->{
+                    try {
+                        players.remove(player);
+                    }catch (Exception ignored){}
+                },60L);
+            }
+        });
+
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e){
+        if(players.contains(e.getPlayer())){
+            e.getPlayer().setVelocity(e.getPlayer().getVelocity().multiply(0));
+        }
+    }
+
+    @EventHandler
+    public void throwProj(ProjectileLaunchEvent e){
+        if(e.getEntity() instanceof Snowball snow){
+            snow.getVelocity().multiply(2);
+        }
+        if(e.getEntity() instanceof ThrownPotion pot){
+            Bukkit.getScheduler().scheduleSyncDelayedTask(SnowBalls.getInstance(),()-> {
+                pot.getVelocity().multiply(4);
+            }, 1L);
+        }
+        Bukkit.broadcastMessage(e.getEntity().getClass().getName());
     }
 }
--- src/main/java/caps123987/snowballs/SnowBalls.java
+++ src/main/java/caps123987/snowballs/SnowBalls.java
@@ -6,15 +6,19 @@ import org.bukkit.Server;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SnowBalls extends JavaPlugin {
-
+    static SnowBalls instance;
     @Override
     public void onEnable() {
+        instance = this;
         Bukkit.getServer().getPluginManager().registerEvents(new SnowListener(), this);
-
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static SnowBalls getInstance(){
+        return instance;
     }
 }
